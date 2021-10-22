12511cbe9f5a55cded802eaf24ff56e58ed1ede0
md-mh <mmahedi49@gmail.com>
2021-10-22 20:32:34 +0600

    module4-solution

---
+++ module4-solution/script.js
@@ -0,0 +1,15 @@
+
+function click() {
+    const text = document.getElementById('text').value;
+    if (text.startsWith('j')) {
+        console.log('Goodbye ' + text);
+    }
+    else if (text.startsWith('J')) {
+        console.log('Goodbye ' + text);
+    } else {
+        console.log('Hello ' + text);
+    }
+
+}
+
+document.getElementById('click').addEventListener("click", click);
